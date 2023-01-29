bde5b916869df6595bb7f9372a9f7297878ca74a
donkey4hot <tractorist666@gmail.com>
2023-01-29 17:16:38 +0300

    init

--- skills.py
+++ skills.py
@@ -1,16 +1,15 @@
 from __future__ import annotations
 from abc import ABC, abstractmethod
-from typing import TYPE_CHECKING
 
-if TYPE_CHECKING:
-    from unit import BaseUnit
 
 class Skill(ABC):
     """
     Базовый класс умения
     """
-    user = None
-    target = None
+
+    def __init__(self):
+        self.user = None
+        self.target = None
 
     @property
     @abstractmethod
@@ -32,37 +31,39 @@ class Skill(ABC):
         pass
 
     def _is_stamina_enough(self):
-        return self.user.stamina > self.stamina
+        return self.user.stamina >= self.stamina
 
-    def use(self, user: BaseUnit, target: BaseUnit) -> str:
+    def use(self, user, target) -> str:
         """
         Проверка, достаточно ли выносливости у игрока для применения умения.
         Для вызова скилла везде используем просто use
         """
         self.user = user
         self.target = target
-        if self._is_stamina_enough:
+        if self._is_stamina_enough():
             return self.skill_effect()
         return f"{self.user.name} попытался использовать {self.name} но у него не хватило выносливости."
 
 
 class FuryPunch(Skill):
-    name = ...
-    stamina = ...
-    damage = ...
+    name = 'Свирепый пинок'
+    stamina = 6
+    damage = 12
 
     def skill_effect(self):
-        # TODO логика использования скилла -> return str
-        # TODO в классе нам доступны экземпляры user и target - можно использовать любые их методы
-        # TODO именно здесь происходит уменшение стамины у игрока применяющего умение и
-        # TODO уменьшение здоровья цели.
-        # TODO результат применения возвращаем строкой
-        pass
+        self.user.stamina -= self.stamina
+        self.target.hp -= self.damage
+
+        return f'{self.user.name} использует {self.name} и наносит {self.damage} урона сопернику.'
+
 
 class HardShot(Skill):
-    name = ...
-    stamina = ...
-    damage = ...
+    name = 'Мощный укол'
+    stamina = 5
+    damage = 15
 
     def skill_effect(self):
-        pass
+        self.user.stamina -= self.stamina
+        self.target.hp -= self.damage
+
+        return f'{self.user.name} использует {self.name} и наносит {self.damage} урона сопернику.'
